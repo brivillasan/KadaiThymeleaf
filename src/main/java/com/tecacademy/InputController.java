e8435643c4f8758bd3bcb14fab1e36754b78cb94
brivillasan <brivillayh@gmail.com>
2023-02-24 19:05:35 +0900

    課題再提出いたします。ご確認のほど、よろしくお願いいたします。

--- src/main/java/com/tecacademy/InputController.java
+++ src/main/java/com/tecacademy/InputController.java
@@ -3,8 +3,6 @@ package com.tecacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -17,7 +15,7 @@ public class InputController {
     }
 
 
-    @GetMapping("/input/{previous}")
+    @GetMapping("/input/")
     public String getPrevious(@RequestParam("previous") String previous, Model model) {
         // input.htmlに画面遷移
         model.addAttribute("previous", previous);
